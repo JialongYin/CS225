12c3d54cc4a0fffc89b1cac5678886969dc87d21
Jialong Yin <jialong2@siebl-0224-25.ews.illinois.edu>
2018-09-16 12:20:01 -0500

    file submission

--- mp2/Image.h
+++ mp2/Image.h
@@ -4,4 +4,177 @@
  */
 
 #pragma once
+#include "cs225/PNG.h"
+using namespace cs225;
 
+class Image : public PNG{
+public:
+  void lighten(){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+        HSLAPixel & pixel = getPixel(x, y);
+        if (pixel.l <= (1 - 0.1))
+        pixel.l = pixel.l + 0.1;
+        else pixel.l = 1;
+      }
+    }
+  }
+
+  void lighten(double amount){
+
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          if (pixel.l <= (1 - amount))
+          pixel.l = pixel.l + amount;
+          else pixel.l = 1;
+      }
+    }
+  }
+
+  void darken(){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+        HSLAPixel & pixel = getPixel(x, y);
+        if (pixel.l >= (0 + 0.1))
+        pixel.l = pixel.l - 0.1;
+        else pixel.l = 0;
+      }
+    }
+  }
+
+  void darken(double amount){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          if (pixel.l >= (0 + amount))
+          pixel.l = pixel.l - amount;
+          else pixel.l = 0;
+      }
+    }
+  }
+
+  void saturate(){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          if (pixel.s <= (1 - 0.1))
+          pixel.s = pixel.s + 0.1;
+          else pixel.s = 1;
+      }
+    }
+  }
+
+  void saturate(double amount){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          if (pixel.s <= (1 - amount))
+          pixel.s = pixel.s + amount;
+          else pixel.s = 1;
+      }
+    }
+  }
+
+  void desaturate(){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          if (pixel.s >= (0 + 0.1))
+          pixel.s = pixel.s - 0.1;
+          else pixel.s = 0;
+      }
+    }
+  }
+
+  void desaturate(double amount){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          if (pixel.s >= (0 + amount))
+          pixel.s = pixel.s - amount;
+          else pixel.s = 0;
+      }
+    }
+  }
+
+  void grayscale(){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+        HSLAPixel & pixel = getPixel(x, y);
+        pixel.s = 0;
+      }
+    }
+  }
+
+  void rotateColor(double degrees){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+        HSLAPixel & pixel = getPixel(x, y);
+        if ((pixel.h + degrees) <= 360 && (pixel.h + degrees) >= 0)
+        pixel.h = pixel.h + degrees;
+        else if ((pixel.h + degrees) >= 360)
+        pixel.h = pixel.h + degrees - 360;
+        else if ((pixel.h + degrees) <= 0)
+        pixel.h = pixel.h + degrees + 360;
+      }
+    }
+  }
+
+
+  void illinify(){
+    for (unsigned x = 0; x < width(); x++) {
+      for (unsigned y = 0; y < height(); y++) {
+
+        HSLAPixel & pixel = getPixel(x, y);
+        if ((pixel.h>113.5)&&(pixel.h<293.5)) {
+          pixel.h=216;
+        } else {
+          pixel.h=11;
+        }
+      }
+    }
+
+  }
+
+
+  void scale(unsigned w, unsigned h){
+    int factor = width() / w;
+    resize(w, h);
+    for (int x = (width() - 1); x >= 0; x--) {
+      for (int y = (height() - 1); y >= 0; y--) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          HSLAPixel & newPixel = getPixel(x / factor, y / factor);
+          pixel = newPixel;
+      }
+    }
+
+  }
+
+
+  void scale(double factor){
+    Image image_copy = *this;
+    resize(width() * factor, height() * factor);
+    for (int x = (width() - 1); x >= 0; x--) {
+      for (int y = (height() - 1); y >= 0; y--) {
+
+          HSLAPixel & pixel = getPixel(x, y);
+          HSLAPixel & newPixel = image_copy.getPixel(x / factor, y / factor);
+          pixel = newPixel;
+      }
+    }
+  }
+
+
+};
--- mp2/StickerSheet.h
+++ mp2/StickerSheet.h
@@ -3,5 +3,33 @@
  * Contains your declaration of the interface for the StickerSheet class.
  */
 #pragma once
+#include "Image.h"
+using namespace cs225;
 
- 
+class StickerSheet{
+public:
+  StickerSheet (const Image &picture, unsigned max){
+    size_max_ = max;
+    images[0] = picture;
+  }
+  ~StickerSheet (){
+    if (images != NULL){
+      delete [] images;
+      images = NULL
+    }
+
+  }
+  StickerSheet (const StickerSheet &other)
+  const StickerSheet & 	operator= (const StickerSheet &other)
+  void 	changeMaxStickers (unsigned max)
+  int 	addSticker (Image &sticker, unsigned x, unsigned y)
+  bool 	translate (unsigned index, unsigned x, unsigned y)
+  void 	removeSticker (unsigned index)
+  Image * 	getSticker (unsigned index) const
+  Image 	render () const
+private:
+  int size_max_;
+  Image *images;
+  images = new Image[size_max_];
+
+};
