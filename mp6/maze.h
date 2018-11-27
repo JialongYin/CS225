c107902c540d164f9946e4a188fa7baae9394223
Jialong Yin <jialong2@gelib-4c-34.ews.illinois.edu>
2018-11-27 13:51:37 -0600

    file submission

--- mp6/maze.h
+++ mp6/maze.h
@@ -1,19 +1,19 @@
 /* Your code here! */
-#pragma once
-#include <vector>
-#include "dsets.h"
-using namespace std;
-class SquareMaze{
-public:
-  SquareMaze ();
-  void makeMaze (int width, int height);
-  bool canTravel (int x, int y, int dir) const;
-  void setWall (int x, int y, int dir, bool exists);
-  vector<int> solveMaze ();
-  PNG *drawMaze () const;
-  PNG *drawMazeWithSolution ();
-private:
-  vector<tuple<int, int>> maze;
-  int width_;
-  int height_;
-};
+// #pragma once
+// #include <vector>
+// #include "dsets.h"
+// using namespace std;
+// class SquareMaze{
+// public:
+//   SquareMaze ();
+//   void makeMaze (int width, int height);
+//   bool canTravel (int x, int y, int dir) const;
+//   void setWall (int x, int y, int dir, bool exists);
+//   vector<int> solveMaze ();
+//   PNG *drawMaze () const;
+//   PNG *drawMazeWithSolution ();
+// private:
+//   vector<tuple<int, int>> maze;
+//   int width_;
+//   int height_;
+// };
